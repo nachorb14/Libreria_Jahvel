6733172862bbeb10c43b28e554359a17fc3b4784
Ignacio Romero Barolin <nachorb2001@gmail.com>
2025-07-25 12:01:08 -0300

    Agregando modelos en ambos admins

--- libreria/admin.py
+++ libreria/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Autor, Libro, Genero
+from .models import Autor, Libro, Genero, Biblia
 
-register_models = [Autor, Libro, Genero]
+register_models = [Autor, Libro, Genero, Biblia]
 
 admin.site.register(register_models)
